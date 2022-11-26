1bf87f2313e6a116d91227b9fab3d11a7d56adaf
AndreaPanzeri <andpanz@libero.it>
2022-11-26 17:53:03 +0100

    card section

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
-    
+    <div class="cards">
+        
+    </div>
   </main>
 </template>
 
@@ -10,6 +12,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
+main{
+    width: 60%;
+    margin: auto;
+}
+
+.cards{
+    background-color: #2e3a46;
+}
 </style>
